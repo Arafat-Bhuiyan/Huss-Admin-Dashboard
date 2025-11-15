3710fe640f9857f4c4e7ae4bf78d1a47b5bd92f2
Arafat-Bhuiyan <arafatbhuiyan41@gmail.com>
2025-11-15 17:23:23 +0600

    wishlist user table page style fix

--- src/pages/Wishlist/WishlistUserListTable.jsx
+++ src/pages/Wishlist/WishlistUserListTable.jsx
@@ -66,7 +66,7 @@ export default function WishlistUserListTable() {
   const isAnySelected = selectedUserIds.length > 0;
 
   return (
-    <>
+    <div className="min-h-screen bg-[#FAF8F2]" >
       <div className="py-8">
         {/* Header */}
         <div className="mb-8">
@@ -167,6 +167,6 @@ export default function WishlistUserListTable() {
         onClose={() => setIsModalOpen(false)}
         user={selectedUser}
       />
-    </>
+    </div>
   );
 }
