5038a3a1a7fe3944f9ecd2d4a51dee24e60b9ad1
Arafat-Bhuiyan <arafatbhuiyan41@gmail.com>
2026-02-23 12:03:55 +0600

    updated code

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
-    host: "10.10.13.12", // your local IP (no leading space)
-    port: 5175, // default Vite dev server port
+  server: {
+    host: true,
+    port: 5175,
   },
 })
